862d80c2cb61ac2eb227c871ed03ff4f8c52d6ca
ongsat <1067357662@qq.com>
2019-12-11 19:50:55 +0800

    :tada: 初始化提交

---
+++ nuxt.config.js
@@ -0,0 +1,36 @@
+
+module.exports = {
+  mode: 'universal',
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  loading: { color: '#fff' },
+  css: [
+    'view-design/dist/styles/iview.css'
+  ],
+  plugins: [
+    '@/plugins/iview'
+  ],
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/dotenv'
+  ],
+  axios: {
+  },
+  build: {
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ plugins/iview.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import ViewUI from 'view-design'
+import zh from 'view-design/dist/locale/zh-CN'
+
+Vue.use(ViewUI, {
+  zh
+})
